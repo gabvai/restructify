84b892496dd7dc72fae26141960d010bc62d3427
D0mce <92205597+D0mce@users.noreply.github.com>
2026-06-06 18:12:41 +0300

    Delete frontend directory

--- frontend/src/components/education/Card.jsx
+++
@@ -1,6 +0,0 @@
-const Card = ({ children, className = "", as: Component = "article" }) => {
-  const classes = ["edu-card", className].filter(Boolean).join(" ");
-  return <Component className={classes}>{children}</Component>;
-};
-
-export default Card;
