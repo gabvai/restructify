825317d232330a52cda3c16c77dd256036aeaf44
D0mce <92205597+D0mce@users.noreply.github.com>
2026-06-06 18:12:51 +0300

    Delete backend directory

--- backend/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-const notFound = (req, _res, next) => {
-  const error = new Error(`Route not found: ${req.method} ${req.originalUrl}`);
-  error.statusCode = 404;
-  next(error);
-};
-
-module.exports = {
-  notFound
-};
--- backend/utils/httpError.js
+++
@@ -1,9 +0,0 @@
-const httpError = (statusCode, message) => {
-  const error = new Error(message);
-  error.statusCode = statusCode;
-  return error;
-};
-
-module.exports = {
-  httpError
-};
